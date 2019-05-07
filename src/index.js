e34ca690e83d7c53b2afa35d34f5457518b47a7a
pac5man <elad@puresec.io>
2019-05-07 11:23:24 +0300

    CR FIX

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 const path = require('path');
-module.exports = require('node-gyp-build')(path.join(__dirname, '..'));
-
+try {
+  module.exports = require('node-gyp-build')(path.join(__dirname, '..'));
+} catch (e) { // For webpack applications
+  module.exports = require('node-gyp-build')(path.join(process.env.LAMBDA_TASK_ROOT, 'src','csocket-linux'));
+}
